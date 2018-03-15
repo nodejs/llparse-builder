951e9d11a94496cb242708121f74bac44f5953b5
Fedor Indutny <fedor@indutny.com>
2018-03-15 13:28:16 -0400

    edge: verify signature

--- src/edge.ts
+++ src/edge.ts
@@ -1,5 +1,7 @@
+import * as assert from 'assert';
+
 import { Buffer } from 'buffer';
-import { Node } from './node';
+import { Invoke, Node } from './node';
 
 export class Edge {
   public static compare(a: Edge, b: Edge): number {
@@ -10,5 +12,17 @@ export class Edge {
               public readonly noAdvance: boolean,
               public readonly key: Buffer | undefined,
               public readonly value: number | undefined) {
+    if (node instanceof Invoke) {
+      if (value === undefined) {
+        assert.strictEqual(node.code.signature, 'match',
+          'Invalid Invoke\'s code signature');
+      } else {
+        assert.strictEqual(node.code.signature, 'value',
+          'Invalid Invoke\'s code signature');
+      }
+    } else {
+      assert.strictEqual(value, undefined,
+        'Attempting to pass value to non-Invoke node');
+    }
   }
 }
