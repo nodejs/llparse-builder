458373a25d694b6a4ceb1eef8da8c4f855f4d5d9
Fedor Indutny <fedor@indutny.com>
2018-03-16 17:39:21 -0400

    pause: support `.otherwise()`

--- src/node/pause.ts
+++ src/node/pause.ts
@@ -7,6 +7,7 @@ export class Pause extends Node {
     assert.strictEqual(code, code | 0, 'code must be integer');
   }
 
-  public otherwise(node: Node): this { throw new Error('Not supported'); }
-  public skipTo(node: Node): this { throw new Error('Not supported'); }
+  public skipTo(node: Node): this {
+    throw new Error('Not supported, please use `pause.otherwise()`');
+  }
 }
