4cdaf4a8bf948c7ea6114455326752b9465a93b3
Vizonex <VizonexBusiness@gmail.com>
2025-07-26 13:15:10 -0500

    add unpack class

--- src/code/unpack.ts
+++ src/code/unpack.ts
@@ -6,20 +6,27 @@ export enum Endianess {
     Big = 1
 };
 
-/** Helps with making unpacking protocols
+/** Helps with making unpacking protocols.
  * It unpacks variables via left & right shifting 
  * based on a given endianness argument
  */
 export class Unpack extends Field {
+    public readonly bigEndian:boolean;
     /**
      * @param field Name of the property that should be unpacked to
      * @param endianness determines how to unpack the value provided
      * Default Value: `Endianess.Little`.
      */
-    constructor (field: string, public readonly endianness?:Endianess){
+    
+    constructor (field: string, endianness?:Endianess){
         if (endianness === undefined){
             endianness = Endianess.Little;
-        }
-        super('match', 'unpack', field);
+        } 
+        super(
+            'match', 
+            endianness == Endianess.Little ? 'unpack_le' : 'unpack_be',  
+            field
+        );
+        this.bigEndian = endianness == Endianess.Big;
     }
 }
